f4a19121948d72c56d46c749dc76d70b32ea2c62
Izaías Araújo <izaiasousa@gmail.com>
2025-01-17 23:34:56 -0300

    refactor: Add keyboard input and buzzer functionality with LED indicators

--- main.c
+++ main.c
@@ -1,101 +1,92 @@
-// Bibliotecas que usaremos no projeto
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-// Macros para usarmos como sonstantes no codigo
-#define NUM_ROW_COLUMNS 4 // Número de linhas e colunas
-#define BUZZER_PIN 28     // Pino do Buzzer
-#define LED_RED 11        // Pino do led vermelho
-#define LED_GREEN 12      // Pino do led verde
-#define LED_BLUE 13       // Pino do led azul
+#define NUM_ROW_COLUMNS 4
+#define BUZZER_PIN 28
+#define LED_RED 11
+#define LED_GREEN 12
+#define LED_BLUE 13
 
-// Definição das funções implementadas no projeto
 void initialize_gpio();
 char read_keyboard();
 void buzzer_beep();
-void Pisca_RED();
-void Pisca_GREEN();
-void Pisca_BLUE();
+void blink_red();
+void blink_green();
+void blink_blue();
 
-// Vetores para as linhas e colunas GPIO do teclado
-uint8_t row_pins[NUM_ROW_COLUMNS] = {2, 3, 4, 5};    // Vetor para as linhas GPIO
-uint8_t column_pins[NUM_ROW_COLUMNS] = {6, 7, 8, 9}; // Vetor para as colunas GPIO
+uint8_t row_pins[NUM_ROW_COLUMNS] = {2, 3, 4, 5};
+uint8_t column_pins[NUM_ROW_COLUMNS] = {6, 7, 8, 9};
 
-// Representação do teclado
 char key_board[NUM_ROW_COLUMNS][NUM_ROW_COLUMNS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
-// Função main
 int main()
 {
-    // Inicialialização dos GPIOs
     initialize_gpio();
     stdio_init_all();
-    printf("Teclado e buzzer inicializados.\n");
+    printf("Keyboard and buzzer initialized.\n");
 
     while (1)
     {
         char key = read_keyboard();
 
-        // Verifica se um botão foi teclado
         if (key)
         {
-            printf("Tecla pressionada: %c\n", key);
+            printf("Key pressed: %c\n", key);
 
-            // Switch case para cada botão do teclado
             switch (key)
             {
             case '1':
-                Pisca_GREEN();
+                blink_green();
                 break;
             case '2':
-                Pisca_BLUE();;
+                blink_blue();
                 break;
             case '3':
-                Pisca_RED();
+                blink_red();
                 break;
             case '4':
-                Pisca_GREEN();
+                blink_green();
                 break;
             case '5':
-                Pisca_BLUE();;
+                blink_blue();
                 break;
             case '6':
-                Pisca_RED();
+                blink_red();
                 break;
             case '7':
-               Pisca_GREEN();
-               break;
+                blink_green();
+                break;
             case '8':
-                Pisca_BLUE();;
+                blink_blue();
                 break;
             case '9':
-                Pisca_RED();
+                blink_red();
                 break;
             case '0':
-               Pisca_GREEN();
-               break;
+                blink_green();
+                break;
             case 'A':
-                Pisca_BLUE();;
+                blink_blue();
                 break;
             case 'B':
-                Pisca_RED();
+                blink_red();
                 break;
             case 'C':
-               Pisca_GREEN();
-               break;
+                blink_green();
+                break;
             case 'D':
-                Pisca_BLUE();
+                blink_blue();
                 break;
             case '#':
-                Pisca_BLUE();
+                blink_blue();
                 break;
             case '*':
-                Pisca_RED();
-                printf("Buzzer acionado!\n");
+                blink_red();
+                printf("Buzzer activated!\n");
                 buzzer_beep();
                 break;
             }
@@ -105,9 +96,8 @@ int main()
     }
 
     return 0;
-} // Fim main
+}
 
-// Função para inicializar os GPIOs
 void initialize_gpio()
 {
     int i;
@@ -126,21 +116,18 @@ void initialize_gpio()
         gpio_pull_down(column_pins[i]);
     }
 
-    // GPIO do Buzzer
     gpio_init(BUZZER_PIN);
     gpio_set_dir(BUZZER_PIN, GPIO_OUT);
     gpio_put(BUZZER_PIN, 0);
 
-    // GPIO do led azul,verde,vermelho
     gpio_init(LED_BLUE);
     gpio_set_dir(LED_BLUE, GPIO_OUT);
     gpio_init(LED_RED);
     gpio_set_dir(LED_RED, GPIO_OUT);
     gpio_init(LED_GREEN);
     gpio_set_dir(LED_GREEN, GPIO_OUT);
-} // Fim initialize_gpio
+}
 
-// Função para leitura do teclado
 char read_keyboard()
 {
     int i, j;
@@ -160,9 +147,8 @@ char read_keyboard()
         gpio_put(row_pins[i], 0);
     }
     return 0;
-} // Fim read_keyboard
+}
 
-// Função para o buzzer(Ainda em andamento)
 void buzzer_beep()
 {
     int contB;
@@ -173,21 +159,21 @@ void buzzer_beep()
         gpio_put(BUZZER_PIN, 0);
         sleep_us(50);
     }
-} // Fim buzzer_beep
+}
 
-void Pisca_RED()
+void blink_red()
 {
     gpio_put(LED_RED, true);
     sleep_ms(500);
     gpio_put(LED_RED, false);
 }
-void Pisca_BLUE()
+void blink_blue()
 {
     gpio_put(LED_BLUE, true);
     sleep_ms(500);
     gpio_put(LED_BLUE, false);
 }
-void Pisca_GREEN()
+void blink_green()
 {
     gpio_put(LED_GREEN, true);
     sleep_ms(500);
